895af508646b36b9015ccd0ec29d0f52b4e146fb
NAITOH Jun <naitoh@gmail.com>
2012-09-03 12:08:30 +0900

    check Mime::pdf for Rails 3.x warning.

--- init.rb
+++ init.rb
@@ -6,5 +6,5 @@ end
 require('cgi')
 require 'rfpdf'
 
-Mime::Type.register "application/pdf", :pdf
+Mime::Type.register "application/pdf", :pdf unless defined? Mime::PDF
 ActionView::Template::register_template_handler 'rfpdf', RFPDF::TemplateHandlers::Base
