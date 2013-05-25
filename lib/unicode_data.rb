1fe120a3530974c3f64b04acbdb76ffefbf52362
NAITOH Jun <naitoh@gmail.com>
2013-05-25 11:53:56 +0900

    unicode_data.rb was updated from tcpdf 4.5.032.

--- lib/unicode_data.rb
+++ lib/unicode_data.rb
@@ -18248,9 +18248,7 @@ module Unicode_data
 1740=>[64508, 64509, 64510, 64511],
 1744=>[64484, 64485, 64486, 64487],
 1746=>[64430, 64431],
-1747=>[64432, 64433],
-8204=>[00000],
-8205=>[00000, 00000, 00000, 00000]
+1747=>[64432, 64433]
 }
 
 #
@@ -18277,6 +18275,40 @@ module Unicode_data
 1615=>64609, # Shadda + Damma
 1616=>64610  # Shadda + Kasra
 }
+
+#
+# Array of character substitutions from UTF-8 unicode to latin1
+#
+@@utf8tolatin = {
+8364=>128, # Euro1
+338=>140,  # OE
+352=>138,  # Scaron
+376=>159,  # Ydieresis
+381=>142,  # Zcaron2
+8226=>149, # bullet3
+710=>136,  # circumflex
+8224=>134, # dagger
+8225=>135, # daggerdbl
+8230=>133, # ellipsis
+8212=>151, # emdash
+8211=>150, # endash
+402=>131,  # florin
+8249=>139, # guilsinglleft
+8250=>155, # guilsinglright
+339=>156,  # oe
+8240=>137, # perthousand
+8222=>132, # quotedblbase
+8220=>147, # quotedblleft
+8221=>148, # quotedblright
+8216=>145, # quoteleft
+8217=>146, # quoteright
+8218=>130, # quotesinglbase
+353=>154,  # scaron
+732=>152,  # tilde
+8482=>153, # trademark
+382=>158   # zcaron2
+}
+
 end
 
 #============================================================+
