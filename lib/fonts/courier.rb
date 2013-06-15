da40eff8dc2c5f31d2f5232373f8f6485b555905
NAITOH Jun <naitoh@gmail.com>
2013-06-15 12:49:51 +0900

    Updates core fonts from tcpdf 4.5.032.

--- lib/fonts/courier.rb
+++ lib/fonts/courier.rb
@@ -1,22 +1,34 @@
 TCPDFFontDescriptor.define('courier') do |font|
+  # core font definition file for TCPDF (www.tcpdf.org)
+  font[:type]='core'
+  font[:dw]=600
   font[:cw] = {}
   0.upto(255) do |i|
   	font[:cw][i]=600
 	end
 end
 TCPDFFontDescriptor.define('courierb') do |font|
+  # core font definition file for TCPDF (www.tcpdf.org)
+  font[:type]='core'
+  font[:dw]=600
   font[:cw] = {}
   0.upto(255) do |i|
   	font[:cw][i]=600
 	end
 end
 TCPDFFontDescriptor.define('courierbi') do |font|
+  # core font definition file for TCPDF (www.tcpdf.org)
+  font[:type]='core'
+  font[:dw]=600
   font[:cw] = {}
   0.upto(255) do |i|
   	font[:cw][i]=600
 	end
 end
 TCPDFFontDescriptor.define('courieri') do |font|
+  # core font definition file for TCPDF (www.tcpdf.org)
+  font[:type]='core'
+  font[:dw]=600
   font[:cw] = {}
   0.upto(255) do |i|
   	font[:cw][i]=600
